89897c29125ba599071059723533f87329c4b209
BenjaminMorris <redbeardben@gmail.com>
2025-09-02 13:49:10 -0700

    specify the right identifier

--- website/outputs.tf
+++ website/outputs.tf
@@ -1,3 +1,3 @@
 output "api_gateway_url" {
-    value = "https://${aws_api_gateway_rest_api.website_api.id}.execute-api.${data.aws_region.current}.amazonaws.com/${aws_api_gateway_stage.api_stage.stage_name}/submit_meet"
+    value = "https://${aws_api_gateway_rest_api.website_api.id}.execute-api.${data.aws_region.current.region}.amazonaws.com/${aws_api_gateway_stage.api_stage.stage_name}/submit_meet"
 }
